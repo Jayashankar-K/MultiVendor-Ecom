77a93d3a0c5301e97beaa5b6c2b87ed093eaf2f7
Jayashankar-K <1shankarkore@gmail.com>
2024-05-21 20:58:52 +0530

    This commit consists of all the initial backend (Database and APIs) for basic initialization of front end

---
+++ backend_api/main/views.py
@@ -0,0 +1,84 @@
+from rest_framework import generics, permissions, pagination, viewsets
+from . import serializers
+from . import models
+
+
+# Vendor
+class VendorList(generics.ListCreateAPIView):
+    queryset = models.Vendor.objects.all()
+    serializer_class = serializers.VendorSerializer
+
+
+class VendorDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = models.Vendor.objects.all()
+    serializer_class = serializers.VendorDetailSerializer
+
+
+# Product
+class ProductList(generics.ListCreateAPIView):
+    queryset = models.Product.objects.all()
+    serializer_class = serializers.ProductListSerializer
+    pagination_classes = pagination.PageNumberPagination
+
+    def get_queryset(self):
+        qs = super().get_queryset()
+        category = self.request.GET["category"]
+        category = models.ProductCategory.objects.get(id=category)
+        qs = qs.filter(category=category)
+        return qs
+
+
+class ProductDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = models.Product.objects.all()
+    serializer_class = serializers.ProductDetailSerializer
+
+
+# Customer
+class CustomerList(generics.ListCreateAPIView):
+    queryset = models.Customer.objects.all()
+    serializer_class = serializers.CustomerSerializer
+
+
+class CustomerDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = models.Customer.objects.all()
+    serializer_class = serializers.CustomerDetailSerializer
+
+
+# Order
+class OrderList(generics.ListCreateAPIView):
+    queryset = models.Order.objects.all()
+    serializer_class = serializers.OrderSerializer
+
+
+class OrderDetail(generics.RetrieveUpdateDestroyAPIView):
+    # queryset = models.OrderItems.objects.all()
+    serializer_class = serializers.OrderDetailSerializer
+
+    def get_queryset(self):
+        order_id = self.kwargs["pk"]
+        order = models.Order.objects.get(id=order_id)
+        order_items = models.OrderItems.objects.filter(order=order)
+        return order_items
+
+
+# Customer Address
+class CustomerAddressViewSet(viewsets.ModelViewSet):
+    serializer_class = serializers.CustomerAddressSerializer
+    queryset = models.CustomerAddress.objects.all()
+
+
+# ProductRating
+class ProductRatingViewSet(viewsets.ModelViewSet):
+    serializer_class = serializers.ProductRatingSerializer
+    queryset = models.ProductRating.objects.all()
+
+
+# Category List
+class CategoryList(generics.ListCreateAPIView):
+    queryset = models.ProductCategory.objects.all()
+    serializer_class = serializers.CategorySerializer
+
+
+class CategoryDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = models.ProductCategory.objects.all()
+    serializer_class = serializers.CategoryDetailSerializer
